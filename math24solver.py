1947b5d21a65b001341a0dda91487f16230c4be2
jnally3 <jnally3@ivytech.edu>
2025-10-21 17:06:26 +0000

    used itertools to make iterating through the permutations more efficient - jn

--- math24solver.py
+++ math24solver.py
@@ -1,41 +1,34 @@
 import sys
 import json
+import itertools
 
 def main():
     try:
         if len(sys.argv) != 5:
-            raise ValueError("Exactly four positive integer arguments are required.")
+            raise ValueError("Exactly four integer arguments are required.")
             
-        num1 = sys.argv[1]
-        num2 = sys.argv[2]
-        num3 = sys.argv[3]
-        num4 = sys.argv[4]
-        if float(num1) == 0 or float(num2) == 0 or float(num3) == 0 or float(num4) == 0:
-            raise ValueError("Numbers cannot be zero.")
-        nums = [num1,num2,num3,num4]
+        nums = [sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4]]
         ops = ['+', '-', '*', '/']
         resultset = set()
-        for a in range(4):
-            for b in range(4):
-                for c in range(4):
-                    for d in range(4):
-                        for e in range(4):
-                            for f in range(4):
-                                for g in range(4):
-                                    if a != c and a != e and a != g and c != e and c != g and e != g:
-                                        estr = '(((' + nums[a] + ops[b] + nums[c] + ')' + ops[d] + nums[e]+ ')'  + ops[f] + nums[g] + ')'
-                                        val = eval(estr)
-                                        if val == 24:
-                                            resultset.add(estr)
         
+        for n1, n2, n3, n4 in itertools.permutations(nums):
+            for op1, op2, op3 in itertools.product(ops, repeat=3):
+                estr = f'((({n1}{op1}{n2}){op2}{n3}){op3}{n4})'
+                try:
+                    val = eval(estr)
+                    if val == 24:
+                        resultset.add(estr)
+                except ZeroDivisionError:                   
+                    continue
+
         if not resultset:
             resultset.add("There are no solutions.")
+        
         output_list = list(resultset)
         print(json.dumps(output_list))
         sys.stdout.flush()
 
     except ValueError as e:
-
         print(f"Error: {e}", file=sys.stderr)
         sys.exit(1)
     except Exception as e:
